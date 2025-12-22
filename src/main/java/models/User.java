c4541ac301ebcd53d5cc4dc6d738824db9303e31
asya <asyatim@mail.ru>
2025-12-23 05:35:25 +0600

    Code refactored.

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,7 +1,17 @@
 package models;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
 @Entity
